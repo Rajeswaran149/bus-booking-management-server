b4b30cf61ad88eb4fa7416f17b4924fe0a2e2691
Rajeswaran <rajeswaran149@gmail.com>
2025-03-19 11:04:21 +0530

    update

--- db.js
+++ db.js
@@ -6,14 +6,4 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY; 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-
-async function getUsers() {
-    const { data, error } = await supabase.from('users').select('*');
-    if (error) {
-        console.error(error);
-    } else {
-        console.log(data);
-    }
-}
-
-getUsers();
+module.exports = supabase;
